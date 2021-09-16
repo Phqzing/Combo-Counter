cb6f3a66fa52a98645ed0ee77558c76b4440fb9a
Phqzing <83136387+Phqzing@users.noreply.github.com>
2021-09-16 20:44:43 +0800

    Create ComboCounterTask.php

---
+++ src/Phqzing/ComboCounterTask.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Phqzing;
+
+use pocketmine\scheduler\Task;
+use pocketmine\Player;
+
+class ComboCounterTask extends Task {
+  
+ private $plugin;
+  
+  public function __construct(ComboCounter $plugin){
+    $this->plugin = $plugin;
+  }
+  
+  public function onRun(int $tick):void {
+  
+    foreach($this->plugin->getServer()->getOnlinePlayers() as $players){
+      if(isset($this->plugin->combo[$players->getName()])){
+        $counter = $this->plugin->getConfig()->get("counter");
+        $counter = str_replace("{combo}", $this->plugin->combo[$players->getName()}, $counter);
+        $players->sendTip($counter);
+      }
+    }
+  }
+}
