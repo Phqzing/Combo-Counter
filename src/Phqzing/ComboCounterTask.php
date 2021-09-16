27dec41e099d8f4ece191edf202a7b9bf034b6fa
Phqzing <83136387+Phqzing@users.noreply.github.com>
2021-09-16 21:23:11 +0800

    Update ComboCounterTask.php

--- src/Phqzing/ComboCounterTask.php
+++ src/Phqzing/ComboCounterTask.php
@@ -3,21 +3,15 @@
 namespace Phqzing;
 
 use pocketmine\scheduler\Task;
-use pocketmine\Player;
+use pocketmine\Server;
 
 class ComboCounterTask extends Task {
   
- private $plugin;
-  
-  public function __construct(ComboCounter $plugin){
-    $this->plugin = $plugin;
-  }
-  
   public function onRun(int $tick){
   
-    foreach($this->plugin->getServer()->getOnlinePlayers() as $players){
+    foreach(Server::getInstance()->getOnlinePlayers() as $players){
       if(isset($this->plugin->combo[$players->getName()])){
-        $counter = $this->plugin->getConfig()->get("counter");
+        $counter = ComboCounter::getInstance()->getConfig()->get("counter");
         $counter = str_replace("{combo}", $this->plugin->combo[$players->getName()], $counter);
         $players->sendTip($counter);
       }
